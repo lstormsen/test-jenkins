656bc44ebbb507c68f3ec4853b1627bdbaa5fce6
lstormsen <lstormsen@gmail.com>
2023-03-15 23:00:48 +0800

    main

--- test-jenkins/src/main/java/org/example/Main.java
+++ test-jenkins/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+       System.out.println("init modify");
         System.out.println("Hello world!");
     }
 }
